012514f30dc6d5d4f9a9a6fd99586d9088b6129d
Kdinatale <90799768+Kdinatale@users.noreply.github.com>
2025-01-26 17:16:27 -0500

    Create Ride attributes

--- src/Ride.java
+++ src/Ride.java
@@ -1,4 +1,37 @@
 
 public class Ride {
-
+	
+	public enum RideType{
+		WATER,
+		COASTER,
+		THRILL,
+		GENERAL,
+		KIDDIE
+	}
+	
+	// Attributes 
+	private String rideId;
+	
+	private String rideName;
+	
+	private RideType rideType;
+	
+	private int dispatchTime;
+	
+	private int ridersPerDispatch;
+	
+	// Constructor 
+	public Ride(String rideId, String rideName, RideType rideType, int dispatchTime, int ridersPerDispatch) {
+		
+		this.rideId = rideId;
+		
+		this.rideName = rideName;
+		
+		this.rideType = rideType;
+		
+		this.dispatchTime = dispatchTime;
+		
+		this.ridersPerDispatch = ridersPerDispatch;
+		
+	}
 }
